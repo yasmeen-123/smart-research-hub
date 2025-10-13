719cc6690710c58452156dd8476b5019885c3fe5
Yasmeen Asghar <153538121+yasmeen-123@users.noreply.github.com>
2025-10-13 11:21:19 +0000

    saved files

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,11 +3,18 @@ const nextConfig = {
   reactStrictMode: true,
   outputFileTracingRoot: __dirname,
 
+  // 👇 Add this block to remove the cross-origin warning
+  experimental: {
+    allowedDevOrigins: ["http://127.0.0.1:3000", "http://localhost:3000"],
+  },
+
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_BASE|| "http://127.0.0.1:8000"}/:path*`,
+        destination: `${
+          process.env.NEXT_PUBLIC_API_BASE || "http://127.0.0.1:8000"
+        }/:path*`,
       },
     ];
   },
