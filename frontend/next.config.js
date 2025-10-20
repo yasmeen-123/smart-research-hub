8ef56e6cd7a057b476e093ac94ba5216be723f6f
Yasmeen Asghar <153538121+yasmeen-123@users.noreply.github.com>
2025-10-20 11:56:46 +0000

    saved files

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,20 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  outputFileTracingRoot: __dirname,
-
-
-  experimental: {
-    allowedDevOrigins: ["http://127.0.0.1:3000", "http://localhost:3000"],
-  },
 
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: `${
-          process.env.NEXT_PUBLIC_API_BASE || "http://127.0.0.1:8000"
-        }/:path*`,
+        destination: "http://127.0.0.1:8000/:path*", 
       },
     ];
   },
